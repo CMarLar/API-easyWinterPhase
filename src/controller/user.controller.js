d896643181e99697a1abd4aa2d5a1375d82ed551
MiguelGeneroso <gene17051996@gmail.com>
2022-12-21 14:13:49 +0100

    servicio y funcionalidad Registro

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -3,7 +3,7 @@ const connection = require("../database");
 const {User} = require("../models/user.model");
 
 
-//postRegister-- COMPARA LAS CONTRASEÑAS, SI SON IGUALES SE AÑADE UN NUEVO USUARIO EN LA BBDD
+//postRegister-- SE AÑADE UN NUEVO USUARIO EN LA BBDD
 
 function postRegister(request,response){
 
@@ -26,10 +26,27 @@ function postRegister(request,response){
         }
     })
 
+}
 
-    
+//postLogin-- SE BUSCA UN USUARIO CON EL CORREO Y LA CONTRASEÑA INDICADAS
 
+function postLogin(request,response){
+
+    console.log(request.body);
+    let sql = "SELECT user_id,password,email,avatar,username FROM user WHERE email = ? AND password = ?";
+    let params = [request.body.email,request.body.password];
+    console.log("PARAMS: " + params);
+
+    connection.query(sql,params, function(error,result){
+        if(error){
+            console.log(error);
+            response.send(error);
+        }else{
+            console.log(result);
+            response.send(result);
+        }
+    })
 }
 
 
-module.exports = {postRegister}
+module.exports = {postRegister,postLogin}
--- src/routers/user.routers.js
+++ src/routers/user.routers.js
@@ -5,6 +5,7 @@ const userCtrl = require("../controller/user.controller");
 
 
 router.post('/register', userCtrl.postRegister); //ruta de registro
+router.post('/login',userCtrl.postLogin);//ruta de login
 
 
 module.exports = router;
