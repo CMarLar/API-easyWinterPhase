b2ef560bb2cd4ca2a1ca2caf9feae6b71fd53eef
beckergirl <116119849+beckergirl@users.noreply.github.com>
2022-12-21 11:28:26 +0100

    actualización text

--- src/controller/text.controller.js
+++ src/controller/text.controller.js
@@ -3,99 +3,24 @@ const { response, request } = require("express");
 const connection = require("../database")
 
 
-function getTextPhase1(request, response) // funciona 
-{
-   let sql;
-    if (request.query.id == null)
-        /* sql = "SELECT * FROM text"; */
+  function getTextPhases(request, response) // funciona 
+ {
+    let sql;
+     if (request.query.id == null)
+         sql = "SELECT * FROM text";
+    else
+         sql = "SELECT * FROM text WHERE text_id="  + request.query.id ;
 
-        sql = "SELECT * FROM text WHERE text_id=1";
-
-    connection.query(sql, function (err, result) 
-   {
-       if (err) 
-          console.log(err);
-        else 
-      {
-           response.send(result);
-       }
-    })
+     connection.query(sql, function (err, result) 
+    {
+        if (err) 
+           console.log(err);
+         else 
+       {
+            response.send(result);
+        }
+     })
 }
 
-function getTextPhase2(request, response) // funciona 
-{
-   let sql;
-    if (request.query.id == null)
-        /* sql = "SELECT * FROM text"; */
 
-        sql = "SELECT * FROM text WHERE text_id=2";
-
-    connection.query(sql, function (err, result) 
-   {
-       if (err) 
-          console.log(err);
-        else 
-      {
-           response.send(result);
-       }
-    })
-}
-
-function getTextPhase8(request, response) // funciona 
-{
-   let sql;
-    if (request.query.id == null)
-        /* sql = "SELECT * FROM text"; */
-
-        sql = "SELECT * FROM text WHERE text_id=3";
-
-    connection.query(sql, function (err, result) 
-   {
-       if (err) 
-          console.log(err);
-        else 
-      {
-           response.send(result);
-       }
-    })
-}
-
-function getTextPhase9a(request, response) // funciona 
-{
-   let sql;
-    if (request.query.id == null)
-        /* sql = "SELECT * FROM text"; */
-
-        sql = "SELECT * FROM text WHERE text_id=4";
-
-    connection.query(sql, function (err, result) 
-   {
-       if (err) 
-          console.log(err);
-        else 
-      {
-           response.send(result);
-       }
-    })
-}
-
-function getTextPhase9b(request, response) // funciona 
-{
-   let sql;
-    if (request.query.id == null)
-        /* sql = "SELECT * FROM text"; */
-
-        sql = "SELECT * FROM text WHERE text_id=5";
-
-    connection.query(sql, function (err, result) 
-   {
-       if (err) 
-          console.log(err);
-        else 
-      {
-           response.send(result);
-       }
-    })
-}
-
- module.exports = {getTextPhase1, getTextPhase2, getTextPhase8, getTextPhase9a, getTextPhase9b};
+ module.exports = {getTextPhases};
--- src/routers/text.routers.js
+++ src/routers/text.routers.js
@@ -3,11 +3,13 @@
  const textCtrl = require("../controller/text.controller");
 
 
-router.get("/phase1:/text_id=1", textCtrl.getTextPhase1);
-router.get("/phase2:/text_id=2", textCtrl.getTextPhase2);
-router.get("/phase8:/text_id=3", textCtrl.getTextPhase8);
-router.get("/phase9:/text_id=4", textCtrl.getTextPhase9a);
-router.get("/phase9:/text_id=5", textCtrl.getTextPhase9b);
+router.get("/phases", textCtrl.getTextPhases)
+
+/* router.get("/phases?id=1", textCtrl.getTextPhases);
+router.get("/phases?id=2", textCtrl.getTextPhases);
+router.get("/phases?id=3", textCtrl.getTextPhases);
+router.get("/phases?id=4", textCtrl.getTextPhases);
+router.get("/phases?id=5", textCtrl.getTextPhases); */
 
 
  module.exports = router;
