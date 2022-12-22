4cbb4c7e782ef62bacad57edb5ced118fba3d564
CMarLar <c.marina.larrosa@gmail.com>
2022-12-22 18:59:28 +0100

    actualización players no funciona

--- src/controller/player.controller.js
+++ src/controller/player.controller.js
@@ -5,16 +5,22 @@ const {Player} = require("../models/player.model");
 function postPlayer(request,response){
 
     console.log(request.body);
-    console.log();
+    console.log(request.body.players[0]);
 
     let newPlayer = new Player (null,request.body.campaign_id,request.body.player_name,null);
 
-    let sql = "INSERT INTO players (`campaign_id`, `player_name`) VALUES (?, ?);"
+    let sql = "INSERT INTO players (house_id,campaign_id, player_name,winterPhaseDone) VALUES ?;"//esto se hace para inserciones múltiples
+
+    let values =[
+        [request.body.players[0].house_id,request.body.players[0].campaign_id,request.body.players[0].player_name,request.body.players[0].winterPhaseDone,],
+        [request.body.players[1].house_id,request.body.players[1].campaign_id,request.body.players[1].player_name,request.body.players[1].winterPhaseDone,],
+        [request.body.players[2].house_id,request.body.players[2].campaign_id,request.body.players[2].player_name,request.body.players[2].winterPhaseDone,],
+    ]
 
     let params = [newPlayer.campaign_id,newPlayer.player_name]
     console.log("DATOS: " + JSON.stringify(newPlayer));
 
-    connection.query(sql,params, function(error,result){
+    connection.query(sql,[values], function(error,result){
         if(error){
             console.log(error);
             response.send(result);
@@ -27,6 +33,32 @@ function postPlayer(request,response){
 }
 
 
+
+/*
+function insertTablaTeachers(){
+    let sql = "INSERT INTO teachers(first_name, last_name) VALUES ?;"
+    let values = [
+        ["Miguel","Generoso Valero"],
+        ["Adrian","Generoso Valero"],
+        ["Diego","Generoso Diaz"],
+        ["Mercedes","Valero Jimenez"],
+        ["Pedro","Languila Sorra"],
+        ["Ana","Perez Martin"],
+        ["Luis","Garcia Vaquero"],
+        ["Miguel","De Cervantes"],
+        ["Miguel","Hernandez"],
+        ["Maria","Magdalena Laputa"]
+    ]
+    connection.query(sql,[values], function(err,result){
+        if(err){
+            console.log(err);
+        }else{
+            console.log("Datos introducidos con exito");
+            console.log(result);
+        }
+    })
+}
+*/
 
 
 module.exports = {postPlayer}
