8002e7f3e47e409f09ba757ea8ff8acc3f9cbd55
CMarLar <c.marina.larrosa@gmail.com>
2022-12-28 19:51:50 +0100

    addnpc funcional front y back

--- src/controller/character.controller.js
+++ src/controller/character.controller.js
@@ -1,7 +1,7 @@
 const connection = require("../database")
 const {Character} = require("../models/character.model");
 
-//POST -> /postChar///HAY QUE MODIFICARLA///
+//POST -> /postChar///HAY QUE MODIFICARLA PARA HACER INSERCIONES MÚLTIPLES///
 function postChar(request,response){
 
     console.log(request.body);
@@ -26,9 +26,54 @@ function postChar(request,response){
 
 }
 
+//GET - muestra todos los personajes
+
+function getChar(request,response){
+
+    console.log(request.body);
+
+    let house_id = request.query.house_id;
+
+    let sql = "SELECT * FROM railway.character WHERE house_id = '" + house_id + "'";
+
+    connection.query(sql, (error,result) =>{
+        if(error){console.log(error);}
+        else
+        {
+            console.log(result);
+
+            if(result)
+            response.send(result);
+
+            else{response.send("-1")}
+        }
+    })
+
+}
 
 
-module.exports = {postChar}    
+function deleteChar(request,response){
+
+    let character_id = request.body.character_id;
+    
+        console.log(request.body);//
+        let sql = "DELETE FROM railway.character WHERE character_id = '" + character_id + "'";
+        console.log(sql); 
+        connection.query(sql, (err, result) =>
+        {
+            if (err) 
+                console.log(err);
+            else 
+            {
+                console.log(result);
+                response.send(result);
+            }
+        })
+    }
+
+
+
+module.exports = {postChar,getChar,deleteChar}    
 
 
 // INSERT INTO `railway`.`character` (`house_id`, `char_name`, `age`, `char_status`, `isMarried`, `marriageGlory`, `courtesyMod`, `role`) VALUES ('74', 'Personaje de prueba', '18', b'1', b'0', '0', '0', 'Escudero');
--- src/routers/character.routers.js
+++ src/routers/character.routers.js
@@ -4,6 +4,8 @@ const charCtrl = require("../controller/character.controller");
 
 router.post('/addnpc',charCtrl.postChar);//nuevo personaje
 router.post('/createhouse',charCtrl.postChar)//nuevo personaje principal y escudero
+router.get("/addnpc",charCtrl.getChar)//recoge personajes de una casa
+router.delete("/addnpc",charCtrl.deleteChar)//borra borra un personaje
 
 
 
