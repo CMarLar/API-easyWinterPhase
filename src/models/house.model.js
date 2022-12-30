115cf107c13d483914123f25518f6a79b1905cd7
MiguelGeneroso <gene17051996@gmail.com>
2022-12-30 18:34:26 +0100

    se añade campo economylevels y modificaciones al controller

--- src/models/house.model.js
+++ src/models/house.model.js
@@ -1,6 +1,6 @@
 class House
 {
-    constructor(house_name, activeChar, holding_name,familyCharacteristic,shield,house_id=null)
+    constructor(house_name, activeChar, holding_name,familyCharacteristic,shield,economyLevels,house_id=null)
     {
         this.house_id = house_id;
         this.house_name = house_name;
@@ -8,6 +8,7 @@ class House
         this.holding_name = holding_name;
         this.familyCharacteristic = familyCharacteristic;
         this.shield = shield;
+        this.economyLevels = economyLevels;
 
     }
 }
