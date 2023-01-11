3969d1ee9e4078f5ac75eebe9b83d3b201ab4f59
MiguelGeneroso <gene17051996@gmail.com>
2023-01-11 18:12:27 +0100

    cc

--- src/controller/adicional.controller.js
+++ src/controller/adicional.controller.js
@@ -60,7 +60,7 @@ function currentcampaignToWinterPhase(request,response){
     console.log(JSON.stringify(request.body));
 
     let sql = "INSERT INTO year (yearNumber,isFirstYear,isLastYear,notes,campaign_id) VALUES (?,?,?,?,?)";
-    let values = [request.body.yearNumber + 1,0,1,request.body.notes,request.body.campaign_id];
+    let values = [request.body.year.yearNumber + 1,0,1,request.body.year.notes,request.body.year.campaign_id];
 
     connection.query(sql,values,function(error,result){
 
@@ -69,18 +69,65 @@ function currentcampaignToWinterPhase(request,response){
             // response.send(result);
         }else{
             console.log(result);
-            response.send(result);
+            console.log("ESTE ES EL BIBIRI BABBIRI");
+            // response.send(result);
             let sql3 = "INSERT INTO railway.character(house_id,year_id,char_name,age,char_status,isMarried,marriageGlory,courtesyMod,railway.character.role,sex) SELECT house_id,?,char_name,age,char_status,isMarried,marriageGlory,courtesyMod,railway.character.role,sex FROM railway.character WHERE year_id = ?;"
-            let values3 = [result.insertId,request.body.year_id];
+            let values3 = [result.insertId,request.body.year.year_id];
             console.log("HOLA HOLITA VECINITO: " + values3);
 
-            connection.query(sql3,values3,function(error,result){
-                if(error){
-                    console.log(error);
-                    // response.send(result);
+            connection.query(sql3,values3,function(error3,result3){
+                if(error3){
+                    console.log(error3);
+                    response.send(error3);
                 }else{
-                    console.log(result);
-                    // response.send(result);
+                    console.log("DROGAS Y MAS DROGAS");
+                    console.log("______________________________________________________");
+                    console.log(result.insertId);
+                    console.log(result3);
+                    
+                    let sql4 = "SELECT * FROM railway.character WHERE house_id = ? AND year_id = ? AND railway.character.char_name = ?";
+
+                    for (let i = 0; i < request.body.mainCharacters.length; i++){
+                        console.log("VUELTA Nº " + i);
+                        values4 = [request.body.mainCharacters[i].house_id,result.insertId,request.body.mainCharacters[i].char_name];
+                        console.log(values4);
+                        
+                        connection.query(sql4,values4,function(error4,result4){
+                            if(error4){
+                                console.log(error4);
+                                // response.send(result);
+                            }else{
+                                console.log(result4);
+                                let sql5 = "UPDATE house SET activeChar = ? WHERE house_id = ?";
+                                let values5 = [result4[0].character_id,result4[0].house_id];
+                                console.log("VALUES[0] " + JSON.stringify(values5));
+
+                                connection.query(sql5,values5,function(error5,result5){
+                                    if(error4){
+                                        console.log(error5);
+                                        // response.send(result);
+                                    }else{
+                                        console.log("?????????????????????");
+                                        console.log("RESULT5: " + result5);
+                                    }
+            
+            
+                                })
+                            }
+    
+    
+                        })
+
+                       console.log("FIN VUELTA Nº " + i); 
+                    }
+
+                    //AQUI TIENES QUE HACER UN SEND
+
+
+
+                    // let sql4 = "SELECT house.house_id,house.activeChar, railway.character.character_id, railway.character.house_id,railway.character.year_id,railway.character.char_name,railway.character.age,railway.character.char_status,railway.character.isMarried,marriageGlory,railway.character.courtesyMod,railway.character.role,railway.character.sex FROM house INNER JOIN railway.character ON (railway.character.character_id = house.activeChar) WHERE year_id = ?";
+                    //CARLOS ES POSIBLE QUE NECESITE ESTA QUERY
+                    
                 }
             })
             
@@ -95,22 +142,23 @@ function currentcampaignToWinterPhase(request,response){
         request.body.isFirstYear = 0;
     }
     
-    let values2 = [null,null,0,null,request.body.yearNumber,request.body.campaign_id];
+    let values2 = [null,null,0,null,request.body.year.yearNumber,request.body.year.campaign_id];
 
     console.log(values2);
-    connection.query(sql2,values2,function(error,result){
-        if(error){
-            console.log(error);
+    connection.query(sql2,values2,function(error2,result2){
+        if(error2){
+            console.log(error2);
             // response.send(result);
         }else{
-            console.log(result);
+            console.log(result2);
             // response.send(result);
         }
     })
+
 //ATENCIOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOON
 //SE HAAAAACEEEEEEEEEEEEEEEEEEEE SABEEEEEEEEEEEEEEEEEEEEEEEEEEEER
 //QUE AQUIIIIIIIIII DEBEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
-//CREARRRSEEEEEEEEEEE LA TABLA RELACION PLAYER YEARS
+//CREARRRSEEEEEEEEEEE LA TABLA RELACION PLAYER YEARS y ACTIVE CHARS YEAR +1
 }
 
 function getHouseAndCharacters(request,response){
